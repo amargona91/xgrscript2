7a1408bf0fbdf5b6c1b1cccfb209f1a2f1e1f1a1
amargona91 <104971289+amargona91@users.noreply.github.com>
2022-05-11 02:32:08 +0700

    Update curl.php

--- curl.php
+++ curl.php
@@ -1,3 +1,84 @@
-206 lines (194 sloc)  11.1 KB
+<?php
+error_reporting ( 0 );
+date_default_timezone_set ( "Asia/Jakarta" );
+membutuhkan __DIR__ . '/vendor/autoload.php' ;
+membutuhkan_sekali __DIR__ . '/userAgent.php' ;
+membutuhkan __DIR__ . '/smshub.php' ;
+   
+    //Kue Acak
+    $ identitas_anonim_id = RandomUUID ( 14 ). '-' . RandomUUID ( 14 ). '-' . RandomUUID ( 8 ). '-' . RandomUUID ( 7 ). '-' . RandomUUID ( 14 );
+    $ identitas_cookie_id = RandomUUID ( 14 ). '-' . RandomUUID ( 15 ). '-' . RandomUUID ( 8 ). '-' . RandomUUID ( 6 ). '-' . RandomUUID ( 14 );
 
-<?phperror_reporting(0);date_default_tiezone_set("Asia/Jakarta");require DIR . '/vendor/autoload.php';require_once DIR.'/userAgent.php';require DIR . '/smshub.php'; //APIKEY SMSHUB$key = ''; if($key == null) { die('Masukan apikey di line 9 goblog!!');}echo '----------- AUTO REFF ONEASET WITH SMSHUB -----------'.PHP_EOL.PHP_EOL;use Curl\Curl;$agent = new userAgent(); $sms = new SMSHub($key);$saldo = $sms->getBalance();echo '[ '.date('H:i:s').' ] Saldo SMSHub: '.$saldo.' RUB'.PHP_EOL;$reff = input('[ '.date('H:i:s').' ] Kode Reff u tod');$jumlah = input('[ '.date('H:i:s').' ] Mau Berapa reff tod');for ($ia=0; $ia < $jumlah; $ia++) { //Random Cookies $identity_anonymous_id = RandomUUID(14).'-'.RandomUUID(14).'-'.RandomUUID(8).'-'.RandomUUID(7).'-'.RandomUUID(14); $identity_cookie_id = RandomUUID(14).'-'.RandomUUID(15).'-'.RandomUUID(8).'-'.RandomUUID(6).'-'.RandomUUID(14); //Random Device $deviceId = vsprintf( '%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex(random_bytes(16)), 4) ); $uuid = RandomUUID(13).'-'.RandomUUID(6).'-'.RandomUUID(8).'-'.RandomUUID(4).'-'.RandomUUID(4).'-'.RandomUUID(4).'-'.RandomUUID(12); $deviceToken = RandomDeviceToken(163); $aId = RandomUUID(32); $ad = RandomUUID(52); //Cookies $encodeIdentities = base64_encode('{"$identity_anonymous_id":"'.$identity_anonymous_id.'","$identity_cookie_id":"'.$identity_cookie_id.'"}'); $encodeCookies = urlencode('sensorsdata2015jssdkcross={"distinct_id":"'.$identity_anonymous_id.'","first_id":"","props":{"$latest_traffic_source_type":"直接流量","$latest_search_keyword":"未取到值_直接打开","$latest_referrer":""},"identities":"'.$encodeIdentities.'","history_login_id":{"name":"","value":""},"$device_id":"'.$deviceId.'"}'); $cookies = $encodeCookies.'; languageCode=in'; $no = $ia+1; echo '-----------------------------------------------------'.PHP_EOL; $getnum = $sms->getNumber('aj', '6', '0', 'any'); if (is_array($getnum)) { $id = $getnum['id']; $num = $getnum['number']; $nomorHP = str_replace('628', '08', $num); echo '[ '.date('H:i:s').' ] Mencoba mendaftar dengan Nomor '.$nomorHP; $curl = new Curl(); $curl->setHeader('Host', 'app.oneaset.co.id'); $curl->setHeader('Accept', 'application/json, text/plain, */*'); $curl->setHeader('countryId', '1'); $curl->setUserAgent('User-Agent', $agent->generate('android')); $curl->setHeader('languageId', '123'); $curl->setHeader('Sec-Fetch-Site', 'same-origin'); $curl->setHeader('Sec-Fetch-Mode', 'cors'); $curl->setHeader('Sec-Fetch-Dest', 'empty'); $curl->setReferrer('Referer', 'https://app.oneaset.co.id/finance/Finance/LandingPage?channel=web_OneAset_activity_financeinvite&referrerCode='.$reff.'&source=outside&ad='.$ad.''); $curl->setHeader('Accept-Language', 'en-US,en;q=0.9'); $curl->setHeader('Cookie', $cookies); $curl->get('https://app.oneaset.co.id/api/app/user/sms/captcha?phoneNumber='.$nomorHP.'&imageCaptcha=&smsBizType=1'); if ($curl->error) { $status = $sms->setStatus($id, 8); echo '[ '.date('H:i:s').' ] Error: ' . $curl->errorCode . ': ' . $curl->errorMessage . "\n"; } else { if($curl->response->success == true) { echo ' -> Sukses Mengirim OTP'.PHP_EOL; echo '[ '.date('H:i:s').' ] Menunggu OTP bos'; $getOTP = 0; do { $curl = new Curl(); $curl->post('https://smshub.org/stubs/handler_api.php', 'action=getCurrentActivations&api_key='.$key.'&order=id&orderBy=asc&start=0&length=10'); $otpBos = json_decode($curl->response)->array[0]->code; $getOTP++; if ($getOTP == 10) { echo "."; $getOTP = 0; } } while (strlen($otpBos) !== 5); echo PHP_EOL.'[ '.date('H:i:s').' ] OTP: '.$otpBos.PHP_EOL; $curl = new Curl(); $curl->setHeader('Host', 'app.oneaset.co.id'); $curl->setHeader('Accept', 'application/json, text/plain, */*'); $curl->setHeader('countryId', '1'); $curl->setHeader('User-Agent', $agent->generate('android')); $curl->setHeader('languageId', '123'); $curl->setHeader('Sec-Fetch-Site', 'same-origin');
+    //Perangkat Acak
+    $ deviceId = vsprintf ( '%s%s-%s-%s-%s-%s%s%s' , str_split ( bin2hex ( random_bytes ( 16 )), 4 ) );
+    $ uuid = RandomUUID ( 13 ). '-' . RandomUUID ( 6 ). '-' . RandomUUID ( 8 ). '-' . RandomUUID ( 4 ). '-' . RandomUUID ( 4 ). '-' . RandomUUID ( 4 ). '-' . RandomUUID ( 12 );
+    $ deviceToken = RandomDeviceToken ( 163 );
+    $ aId = RandomUUID ( 32 );
+    $ iklan = RandomUUID ( 52 );
+
+
+    //Kue
+    $ encodeIdentities = base64_encode ( '{"$identity_anonymous_id":"' . $ identity_anonymous_id . '","$identity_cookie_id":"' . $ identitas_cookie_id . '"}' );
+    $ encodeCookies = urlencode ( 'sensorsdata2015jssdkcross={"distinct_id":"' . $ identitas_anonymous_id . '","first_id":"","props":{"$latest_traffic_source_type":"直接流量","$latest_search_keyword":" _直接打开","$latest_referrer":""},"identities":"' . $ encodeIdentities . '","history_login_id":{"name":"","value":""} ,"$device_id":"' . $ deviceId . '"}' );
+    $ cookies = $ encodeCookies . '; kode bahasa=dalam' ;
+
+    $ tidak = $ ia + 1 ;
+    gema  '------------------------------------------------ -----' . PHP_EOL ;
+    $ getnum = $ sms -> getNumber ( 'aj' , '6' , '0' , 'any' );
+    if ( is_array ( $ getnum )) {
+        $ id = $ getnum [ 'id' ];
+        $ angka = $ getnum [ 'angka' ];
+        $ nomorHP = str_replace ( '628' , '08' , $ num );
+        gema  '[ ' . tanggal ( 'H:i:s' ). ' ] Coba Mendaftar dengan Nomor ' . $ nomorHP ;
+
+        $ ikal = ikal baru  ();
+        $ curl -> setHeader ( 'Host' , 'app.oneaset.co.id' );
+        $ curl -> setHeader ( 'Terima' , 'aplikasi/json, teks/polos, */*' );
+        $ curl -> setHeader ( 'countryId' , '1' );
+        $ curl -> setUserAgent ( 'User-Agent' , $ agent -> generate ( 'android' ));
+        $ curl -> setHeader ( 'languageId' , '123' );
+        $ curl -> setHeader ( 'Sec-Fetch-Site' , 'same-origin' );
+        $ curl -> setHeader ( 'Sec-Fetch-Mode' , 'cors' );
+        $ curl -> setHeader ( 'Sec-Fetch-Dest' , 'kosong' );
+        $ curl -> setReferrer ( 'Referer' , 'https://app.oneaset.co.id/finance/Finance/LandingPage?channel=web_OneAset_activity_financeinvite&referrerCode=' . $ reff . '&source=outside&ad=' . $ ad . '' );
+        $ curl -> setHeader ( 'Accept-Language' , 'en-US,en;q=0.9' );
+        $ curl -> setHeader ( 'Cookie' , $ cookies );
+        $ curl -> get ( 'https://app.oneaset.co.id/api/app/user/sms/captcha?phoneNumber=' . $ nomorHP . '&imageCaptcha=&smsBizType=1' );
+        if ( $ curl -> error ) {
+            $ status = $ sms -> setStatus ( $ id , 8 );
+            gema  '[ ' . tanggal ( 'H:i:s' ). ' ] Kesalahan: ' . $ curl -> errorCode . ':' . $ curl -> errorMessage . "\n" ;
+        } lain {
+            if ( $ curl -> response -> success == true ) {
+                echo  ' -> Berhasil Mengirim OTP' . PHP_EOL ;
+                gema  '[ ' . tanggal ( 'H:i:s' ). ' ] Menunggu OTP bos' ;
+                $ getOTP = 0 ;
+                lakukan {
+                    $ ikal = ikal baru  ();
+                    $ curl -> post ( 'https://smshub.org/stubs/handler_api.php' , 'action=getCurrentActivations&api_key=' . $ key . '&order=id&orderBy=asc&start=0&length=10' );
+                    $ otpBos = json_decode ( $ curl -> response )-> array [ 0 ]-> code ;
+                    $ getOTP ++;
+                        if ( $ getOTP == 10 ) {
+                            gema  "." ;
+                            $ getOTP = 0 ;
+                        }
+                
+
+fungsi  RandomDeviceToken ( $ panjang = 10 ) {
+    $ karakter = '0123456789abcdefghijklmnopqrstvwxyzABCDEFGHIJKLMNOPQRSTVWXYZ-_:' ;
+    $ karakterPanjang = strlen ( $ karakter );
+    $ randomString = '' ;
+    for ( $ i = 0 ; $ i < $ length ; $ i ++) {
+        $ randomString .= $ character [ rand ( 0 , $ characterLength - 1 )];
+    }
+    kembalikan  $ randomString ;
+}
+
+fungsi  RandomUUID ( $ panjang = 10 ) {
+    $ karakter = '0123456789abcdefghijklmnopqrstvwxyz' ;
+    $ karakterPanjang = strlen ( $ karakter );
+    $ randomString = '' ;
+    for ( $ i = 0 ; $ i < $ length ; $ i ++) {
+        $ randomString .= $ character [ rand ( 0 , $ characterLength - 1 )];
+    }
+    kembalikan  $ randomString ;
+}
